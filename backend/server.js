cbed77879c7cc6ac02234b3b50618abc06a12344
Ayush Kumar <aayushkumar1228@gmail.com>
2025-11-13 01:11:11 +0530

    add authentication routes and controllers for user login, register, and logout

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
 
 connectDB();
 
 app.listen(3000, () => {
     console.log('⚙️ Server is running on port:3000');
-})
+});
--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function connectDB() {
-    mongoose.connect("mongodb://127.0.0.1:27017/savoroapp")
+    mongoose.connect(process.env.MONGODB_URI)
         .then(() => {
             console.log('MongoDB Connected');
         })
